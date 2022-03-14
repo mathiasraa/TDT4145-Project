0c7c9eb02a5eef225733483dcca2f9c885327e7b
Mathias <skraagen@hotmail.com>
2022-03-14 13:29:51 +0100

    load initial data python

---
+++ initialdata.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+  "main"."Land" ("LandID", "Navn")
+VALUES
+  ('1', 'El Salvador');
+
+--
+INSERT INTO
+  "main"."Land" ("LandID", "Navn")
+VALUES
+  ('2', 'Colombia');
+
+--
+INSERT INTO
+  "main"."Land" ("LandID", "Navn")
+VALUES
+  ('3', 'Rwanda');
+
+--
