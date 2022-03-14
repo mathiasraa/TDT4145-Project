08421d1c5afb9d4a82b924a3d223b70c74ad018a
Sindre Haugland <sindrehaugland@dhcp-10-24-4-53.wlan.ntnu.no>
2022-03-14 14:04:02 +0100

    initial data

--- initialdata.sql
+++ initialdata.sql
@@ -16,3 +16,47 @@ VALUES
   ('3', 'Rwanda');
 
 --
+
+INSERT INTO
+  "main"."KaffebønneArt"("Navn")
+VALUES
+  ("coffea arabica")
+
+--
+
+INSERT INTO
+  "main"."KaffebønneArt"("Navn")
+VALUES
+  ("coffea robusta")
+
+--
+
+INSERT INTO
+  "main"."KaffebønneArt"("Navn")
+VALUES
+  ("coffea liberica")
+
+--
+
+INSERT INTO
+  "main"."Brenneri"("Navn")
+VALUES
+  ("Jacobsen & Svart")
+
+--
+
+
+INSERT INTO
+  "main"."Gård"("Navn", "Høyde", "Land_LandID", "Region_RegionID")
+VALUES
+  ("Nombre de Dios", "1500", "1", "1")
+
+INSERT INTO
+  "main"."Foredlingsmet"("Land_LandID", "Navn")
+VALUES
+  ("1", "Santa Ana")
+
+--
+
+
+
